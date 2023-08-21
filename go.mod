2299675ad2720d7b3f2ee3ef69318f0d756fcdde
Jeff Mendoza <jlm@jlm.name>
2023-08-21 14:36:24 -0700

    test

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/jeffmendoza/test-gha-oidc
 
 go 1.20
 
-require github.com/sigstore/cosign/v2 v2.1.1 // indirect
+require (
+	github.com/sigstore/cosign/v2 v2.1.1
+	golang.org/x/oauth2 v0.9.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.11.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 
 	"github.com/sigstore/cosign/v2/pkg/providers"
 	_ "github.com/sigstore/cosign/v2/pkg/providers/github"
+	"golang.org/x/oauth2"
 )
 
 func main() {
+	if len(os.Args) != 3 {
+		log.Fatalf("Invalid args")
+	}
+
+	tokenURL := os.Args[1]
+	clientID := os.Args[2]
+
 	ctx := context.Background()
 	if !providers.Enabled(ctx) {
 		log.Fatalf("incorrect environment")
@@ -20,5 +29,42 @@ func main() {
 	if token == "" {
 		log.Fatalf("empty token")
 	}
-	log.Printf("Token aquired")
+	log.Printf("ID Token aquired")
+
+	var conf oauth2.Config
+	conf.Endpoint.TokenURL = tokenURL
+	options := []oauth2.AuthCodeOption{
+		oauth2.SetAuthURLParam("grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer"),
+		oauth2.SetAuthURLParam("assertion", token),
+		oauth2.SetAuthURLParam("scope", "openid"),
+		oauth2.SetAuthURLParam("client_id", clientID),
+	}
+	tok, err := conf.Exchange(ctx, "", options...)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	if tok.AccessToken == "" {
+		log.Fatalf("empty token")
+	}
+	log.Printf("access Token aquired")
+
+	// var scopes []string
+	// var audience string
+	// var ctx context.Context
+	// var conf oauth2.Config
+	// var tok oauth2.Token
+	// //... fill in scopes, audience, ctx, conf, and get original access token in tok ...
+	// options := []oauth2.AuthCodeOption{
+	// 	oauth2.SetAuthURLParam("grant_type", "urn:ietf:params:oauth:grant-type:token-exchange"),
+	// 	oauth2.SetAuthURLParam("subject_token", tok.AccessToken),
+	// 	oauth2.SetAuthURLParam("subject_token_type", "urn:ietf:params:oauth:token-type:access_token"),
+	// }
+	// if len(scopes) > 0 {
+	// 	options = append(options, oauth2.SetAuthURLParam("scope", strings.Join(scopes, " ")))
+	// }
+	// if audiences != "" {
+	// 	options = append(options, oauth2.SetAuthURLParam("audience", audience))
+	// }
+	// tok, err = conf.Exchange(ctx, "", options...)
+
 }
